cf2244d644dd8d18327f87ded82d8ba9f05e0ba7
Ahmed <ahmedkhan895.ak@gmail.com>
2024-05-08 16:26:36 +0100

    added funcion to generate random pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,64 @@ import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export default function App() {
-  //initialise pokemon array
-  let [pokemons, setPokemons] = useState([]);
-  //initialise round number
-  let [round, setRound] = useState(1);
+  const [pokemons, setPokemons] = useState([]);
+  const [round, setRound] = useState(0);
+  const [pokemonIds, setPokemonIds] = useState([]);
 
-  const url = "https://pokeapi.co/api/v2/pokemon/25";
+  function getRandomPokemonId() {
+    if (pokemonIds.length === 0) {
+      const uniqueNumbers = new Set();
+      while (uniqueNumbers.size < 6) {
+        const randomNum = Math.floor(Math.random() * 100) + 1;
+        uniqueNumbers.add(randomNum);
+      }
+      return Array.from(uniqueNumbers);
+    }
+    return pokemonIds; // Avoid generating new IDs if already set
+  }
 
-  //function to add pokemon to array
-  const addPokemon = (name, imageUrl) => {
-    const newPokemon = {
-      id: uuidv4(),
-      name: name,
-      imageUrl: imageUrl,
-    };
-    setPokemons([...pokemons, newPokemon]);
+  const fetchInitialPokemons = async () => {
+    const newPokemons = [];
+    const uniquePokemonIds = getRandomPokemonId();
+
+    // Fetch Pokémon data for each unique ID
+    for (const id of uniquePokemonIds) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+      const response = await fetch(url);
+      const data = await response.json();
+
+      newPokemons.push({
+        id: uuidv4(), // Generate unique ID for internal data
+        name: data.name,
+        imageUrl: data.sprites.other.dream_world.front_default,
+        clicked: 0,
+      });
+    }
+
+    setPokemons(newPokemons);
+    setPokemonIds(uniquePokemonIds);
   };
 
   useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) =>
-        addPokemon(data.name, data.sprites.other.dream_world.front_default)
-      );
-  }, []);
+    fetchInitialPokemons();
+  }, []); // Ensures this runs only once on mount
+
+  function handleClick() {
+    setRound(round + 1); // This won't cause fetch on component mount
+  }
 
   console.log(pokemons);
+  console.log(pokemonIds);
 
-  return <></>;
+  return (
+    <>
+      <button onClick={handleClick}>Add Pokémon</button>
+      {pokemons.map((pokemon) => (
+        <div key={pokemon.id}>
+          <img src={pokemon.imageUrl} alt={pokemon.name} />
+          <p>{pokemon.name}</p>
+        </div>
+      ))}
+    </>
+  );
 }
