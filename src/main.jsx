1b49b816a21e407379aed4d409a56f7ee44a4cd1
Ahmed <ahmedkhan895.ak@gmail.com>
2024-05-11 19:03:51 +0100

    changed file structure, put game logic in sperate module

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles/App.scss";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <section className="main">
+    <App />
+  </section>
+);
