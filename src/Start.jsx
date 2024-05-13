a978fd8e4bf5e8ab798df60ca5201681ebe61d43
Ahmed <ahmedkhan895.ak@gmail.com>
2024-05-13 17:58:57 +0100

    finished styling of start page

--- src/Start.jsx
+++ src/Start.jsx
@@ -4,7 +4,17 @@ import "./styles/App.scss";
 export default function Start() {
   return (
     <>
-      <div className="modal">test</div>
+      <div className="start-screen">
+        <div className="modal">
+          <h2>Welcome to the PokéMemo</h2>
+          <p>
+            Challenge your memory by clicking on different Pokémon cards each
+            round. Avoid clicking on the same Pokémon twice, or it's game over.
+          </p>
+          <p>Think you can remember them all? Let's find out!</p>
+          <button className="start-button">Start</button>
+        </div>
+      </div>
     </>
   );
 }
